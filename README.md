818ad0f1933ec1e9cc9319b360943b5d4e6b7a79
PlanC <planc2333@outlook.com>
2021-04-01 15:56:02 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+<!--
+ * @Author: PlanC
+ * @Date: 2021-04-01 15:32:31
+ * @LastEditTime: 2021-04-01 15:39:04
+ * @FilePath: \iplanc.github.io\README.md
+-->
+
 # iplanc.github.io
+[https://iplanc.github.io](https://iplanc.github.io)
--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 /*
  * @Author: PlanC
  * @Date: 2021-01-12 12:33:49
- * @LastEditTime: 2021-01-20 10:21:49
+ * @LastEditTime: 2021-04-01 15:36:48
  * @FilePath: \iplanc.github.io\config.js
  */
 
-var lover = "雨雨";
+var lover = '雨雨';
+var loverNickName = 'Unii';
+var me = '笨比';
+var meNickName = 'PlanC';
 
-var start = new Date("2020-11-11 22:30:00"); //2020-11-11 22:30:00
-//var end = new Date("9999-12-31"); //9999-12-31
+var start = new Date('2020-11-11 22:30:00'); //2020-11-11 22:30:00
+//var end = new Date('9999-12-31'); //9999-12-31
